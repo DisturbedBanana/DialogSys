5d091e259e0f44b4788db814f661e5e1a329f983
DisturbedBanana <tompouts@gmail.com>
2024-01-25 02:22:52 +0100

    [ADD] Magnifying lens logic implemented

--- Assets/Scripts/MagnifyingLensCombinations.cs
+++ Assets/Scripts/MagnifyingLensCombinations.cs
@@ -7,4 +7,29 @@ using NaughtyAttributes;
 public class MagnifyingLensCombinations : ScriptableObject
 {
     public List<LensCombination> CombinationsList = new List<LensCombination>();
+
+    public bool CheckCombination(List<string> currentCombination)
+    {
+        if (currentCombination[0] == currentCombination[1])
+        {
+            Debug.Log("Can't combine the same item");
+            return false;
+        }
+
+        foreach (LensCombination combination in CombinationsList)
+        {
+            if (combination.ItemTags.Count != currentCombination.Count) continue;
+            bool isCombination = true;
+            for (int i = 0; i < combination.ItemTags.Count; i++)
+            {
+                if (!combination.ItemTags.Contains(currentCombination[i]))
+                {
+                    isCombination = false;
+                    break;
+                }
+            }
+            if (isCombination) return true;
+        }
+        return false;
+    }
 }
--- Assets/Scripts/TouchManager.cs
+++ Assets/Scripts/TouchManager.cs
@@ -10,8 +10,15 @@ public class TouchManager : MonoBehaviour
     [Header("Parameters")]
     [SerializeField] float _checkSize;
     [SerializeField] LayerMask _objectLayer;
+    [SerializeField] MagnifyingLensCombinations _magnifyingLensCombinations;
 
     InteractableObject _selectedObject;
+
+    private bool _isUsingLens = false;
+    private SpriteRenderer _lensSprite;
+    private bool _isPreviewingObject = false;
+
+    private List<string> _currentCombination = new List<string>();
 
     public static TouchManager Instance { get; private set; }
 
@@ -39,8 +46,47 @@ public class TouchManager : MonoBehaviour
         Collider2D collider2D = GetClosestCollider(Physics2D.OverlapCircleAll(fingerPosition, _checkSize, _objectLayer), fingerPosition);
         if (collider2D == null) return;
         _selectedObject = collider2D.GetComponent<InteractableObject>();
-        Debug.Log("Selected object: " + _selectedObject.name);
-        _selectedObject.GetComponent<SpriteRenderer>().color = Color.red;
+        if (_isUsingLens)
+        {
+            if (_selectedObject.gameObject.CompareTag("Lens"))
+            {
+                _selectedObject.GetComponent<SpriteRenderer>().color = Color.white;
+                _isUsingLens = false; _currentCombination.Clear(); 
+                return;
+            }
+
+            _currentCombination.Add(_selectedObject.gameObject.tag);
+            if (_currentCombination.Count == 2)
+            {
+                if (_magnifyingLensCombinations.CheckCombination(_currentCombination))
+                {
+                    Debug.Log("Combination found");
+                }
+                else
+                {
+                    Debug.Log("Combination not found");
+                }
+                _isUsingLens = false;
+                _lensSprite.color = Color.white;
+                _currentCombination.Clear();
+            }
+            
+        }
+        else
+        {
+            if (_selectedObject.gameObject.CompareTag("Lens"))
+            {
+                _lensSprite = _selectedObject.GetComponent<SpriteRenderer>();
+                _isUsingLens = true;
+                _lensSprite.color = Color.red;
+            }
+            else
+            {
+                _isPreviewingObject = true;
+            }
+        }
+
+
     }
 
     private void OnInputStopped(Finger finger)
